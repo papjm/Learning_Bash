cfc1f5d7911f0a050233bfca0b6895ff77f50398
papjm <102404994+papjm@users.noreply.github.com>
2023-05-26 16:15:00 +0100

    Update and rename intro to intro.sh

--- intro.sh
+++ intro.sh
@@ -3,3 +3,11 @@ which bash ........... to see the bash available
 use ">>" to append append to another file a single ">" will completely overwrite the existing content of a file
 to comment some lines of text do :''      put all comments inside of the single quote
 || is another syntax for "or" statment can be used in place of "or"
+
+
+some bash
+#!
+
+car=('BMW', 'Mercedes', 'Mclaren')
+
+echo ${car[0]}
