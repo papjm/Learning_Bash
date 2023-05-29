543090ed10551bcf4196e667b6761fa5a55df4c8
papjm <102404994+papjm@users.noreply.github.com>
2023-05-29 16:54:23 +0100

    Update emails.sh

--- emails.sh
+++ emails.sh
@@ -2,3 +2,4 @@ to send emails using bash do the following:
 sudo gedit /etc/ssmtp/ssmtp.conf 
 root=testemail@gmail.com
 mailhub=smtp.gmail.com:587       587 is the TLS port number
+AutUser=testemail@gmail.com
